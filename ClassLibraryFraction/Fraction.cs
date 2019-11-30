825eb88e5560008e2952a407bcc3c67663b9d8f0
Andrey Anchis <andreyanchis@gmail.com>
2019-11-30 22:16:30 +0200

    Работает все, кроме операций сравнения, частично работают операции ++ и --

---
+++ ClassLibraryFraction/Fraction.cs
@@ -0,0 +1,228 @@
+﻿using System;
+
+namespace ClassLibraryFraction
+{
+    public class Fraction
+    {
+        protected long Numerator;
+        protected long Denominator;
+
+        public Fraction(long numer, long denom)
+        {
+            Numerator = numer;
+            Denominator = denom;
+        }
+        public Fraction(long numer)
+        {
+            Numerator = numer;
+            Denominator = 1;
+        }
+        public void Fix()
+        {
+            if ((Numerator <= 0 && Denominator <= 0) || (Numerator >= 0 && Denominator <= 0))
+            {
+                Numerator = -Numerator;
+                Denominator = -Denominator;
+            }
+        }
+        public void Simplify()
+        {
+            long i = 0;
+            long lcm = 0;
+            
+            if (Numerator > Denominator)
+            {
+                i = Denominator;
+            }
+            else i = Numerator;
+
+            while (i > 0 && lcm == 0)
+            {
+                if ((Numerator % i == 0) && (Denominator % i == 0))
+                    lcm = i;
+                i--;
+            }
+            Numerator = Numerator / lcm;
+            Denominator = Denominator / lcm;
+
+            
+        }
+
+        // Перегружаем унарный оператор ++
+        public static Fraction operator ++(Fraction a)
+        {
+            Fraction res = new Fraction(a.Numerator + a.Denominator, a.Denominator);
+            return res;
+        }
+
+        // Перегружаем унарный оператор --
+        public static Fraction operator --(Fraction a)
+        {
+                Fraction res = new Fraction(a.Numerator - a.Denominator, a.Denominator);
+                return res;
+        }
+
+        // Перегружаем унарный оператор -
+        public static Fraction operator -(Fraction a)
+        {
+            return new Fraction(-a.Numerator, a.Denominator);
+        }
+
+        // Перегружаем унарный оператор !
+        public static Fraction operator !(Fraction a)
+        {
+            Fraction res = new Fraction(a.Denominator, a.Numerator);
+            res.Fix();
+            return res;
+        }
+
+        // Перегружаем бинарный оператор +
+        public static Fraction operator +(Fraction a, Fraction b)
+        {
+            Fraction res = new Fraction(a.Numerator * b.Denominator + a.Denominator * b.Numerator, a.Denominator * b.Denominator);
+            res.Simplify();
+            return res;
+        }
+        public static Fraction operator +(Fraction a, long b)
+        {
+            Fraction res = a + new Fraction(b);
+            res.Simplify();
+            return res;
+        }
+        public static Fraction operator +(long a, Fraction b)
+        {
+            Fraction res = new Fraction(a) + b;
+            res.Simplify();
+            return res;
+        }
+
+        // Перегружаем бинарный оператор -
+        public static Fraction operator -(Fraction a, Fraction b)
+        {
+            Fraction res = new Fraction(a.Numerator * b.Denominator - a.Denominator * b.Numerator, a.Denominator * b.Denominator);
+            res.Simplify();
+            return res;
+        }
+        public static Fraction operator -(Fraction a, long b)
+        {
+            Fraction res = a - new Fraction(b);
+            res.Simplify();
+            return res;
+        }
+        public static Fraction operator -(long a, Fraction b)
+        {
+            Fraction res = new Fraction(a) - b;
+            res.Simplify();
+            return res;
+        }
+
+        // Перегружаем бинарный оператор *
+        public static Fraction operator *(Fraction a, Fraction b)
+        {
+            Fraction res = new Fraction(a.Numerator * b.Numerator, a.Denominator * b.Denominator);
+            res.Simplify();
+            return res;
+        }
+        public static Fraction operator *(Fraction a, long b)
+        {
+            Fraction res = a * new Fraction(b);
+            res.Simplify();
+            return res;
+        }
+        public static Fraction operator *(long a, Fraction b)
+        {
+            Fraction res = new Fraction(a) * b;
+            res.Simplify();
+            return res;
+        }
+
+        // Перегружаем бинарный оператор /
+        public static Fraction operator /(Fraction a, Fraction b)
+        {
+            Fraction res = a * !b;
+            res.Simplify();
+            return res;
+        }
+        public static Fraction operator /(Fraction a, long b)
+        {
+            Fraction res = a * !new Fraction(b);
+            res.Simplify();
+            return res;
+        }
+        public static Fraction operator /(long a, Fraction b)
+        {
+            Fraction res = new Fraction(a) * !b;
+            res.Simplify();
+            return res;
+        }
+        // Перегружаем логический оператор ==
+        public bool Equals(Fraction a)
+        {
+            if (a == null)
+                return false;
+
+            return a.Numerator == this.Numerator && a.Denominator == this.Denominator;
+        }
+        /*
+        public static bool operator ==(Fraction a, Fraction b)
+        {
+            if (a.Numerator == b.Numerator && a.Denominator == b.Denominator)
+                return true;
+            return false;
+        }
+
+        // Oбязательно перегрузить логический оператор !=
+        public static bool operator !=(Fraction a, Fraction b)
+        {
+            if (a.Denominator != b.Denominator)
+                return false;
+            return true;
+        }*/
+
+        // Перегружаем логический оператор >=
+        public static bool operator >(Fraction a, Fraction b)
+        {
+            if (a.Denominator != b.Denominator)
+            {
+                a.Numerator = a.Numerator * b.Denominator;
+                a.Denominator = a.Denominator * b.Denominator;
+                b.Numerator = b.Numerator * a.Denominator;
+                b.Denominator = a.Denominator * b.Denominator;
+            }
+            a.Simplify();
+            b.Simplify();
+            if (a.Numerator > b.Numerator)
+                return true;
+            return false;
+        }
+
+        // Oбязательно перегрузить логический оператор <=
+        public static bool operator <(Fraction a, Fraction b)
+        {
+            if (a.Denominator != b.Denominator)
+            {
+                a.Numerator = a.Numerator * b.Denominator;
+                a.Denominator = a.Denominator * b.Denominator;
+                b.Numerator = b.Numerator * a.Denominator;
+                b.Denominator = a.Denominator * b.Denominator;
+            }
+            a.Simplify();
+            b.Simplify();
+            if (a.Numerator < b.Numerator)
+                return false;
+            return true;
+        }
+
+        // Переопределяем метод ToString()
+        public override string ToString()
+        {
+            return $"{Numerator}/{Denominator}";
+        }
+
+        // Использование явного преобразования в тип double
+        public static explicit operator double(Fraction a)
+        {
+            return (double)a.Numerator / a.Denominator;
+        }  
+    }
+}
---
+++ fractions/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using ClassLibraryFraction;
+
+namespace fractions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fraction a = new Fraction(5, 12);
+            Fraction b = new Fraction(2, 6);
+            Fraction c = new Fraction(5, 13);
+            if (a == c)
+                Console.WriteLine("Объекты равны");
+            else
+                Console.WriteLine("Объекты не равны");
+
+            if (a > c)
+                Console.WriteLine("a > c");
+            else if (a < c)
+                Console.WriteLine("a < c");
+                
+            Fraction res1 = a * b;
+            double res_double = (double)res1;
+            Console.WriteLine(res1);
+            Console.WriteLine(res_double);
+
+            Fraction res2 = a / !b;
+            Console.WriteLine(res2);
+
+            Fraction res3 = a - (-b);
+            Console.WriteLine(res3);
+
+            Fraction res4 = ++a;
+            Console.WriteLine(res4);
+
+            
+        }
+    }
+}
